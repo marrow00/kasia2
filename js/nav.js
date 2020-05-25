92cf9ab466c053a1f42f92b5c2aa7bca0608c558
Unknown <skrzynska.konstancja@gmail.com>
2020-05-25 12:01:49 +0200

    close menu after click on link

--- js/nav.js
+++ js/nav.js
@@ -1,7 +1,15 @@
 const menuButton = document.getElementById('menu-button');
+const menuSpan = Array.from(menuButton.children);
 const menu = document.getElementById('menu');
+const links = Array.from(menu.children)
 menuButton.addEventListener('click',function(){
-    Array.from(this.children).forEach(e => e.classList.toggle('menu-open'))
+    menuSpan.forEach(e => e.classList.toggle('menu-open'))
     menu.classList.toggle('active');
+})
+links.forEach(link => {
+    link.addEventListener('click', function(){
+        menu.classList.remove('active');
+        menuSpan.forEach(e => e.classList.remove('menu-open'))
+    })
 })
    
